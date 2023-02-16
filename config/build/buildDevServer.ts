4a0c1ad9b9dd8436b37dce83f1c699ccd98bf9e8
AlexandrPristavka <alexandr.pristavka@winfinity.live>
2023-02-16 22:05:45 +0300

    16 Webpack hot module replacement

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,10 +1,13 @@
-import {IBuildOptions} from "./types/config";
-import {Configuration as DevServerConfiguration} from "webpack-dev-server";
+import { IBuildOptions } from "./types/config";
+import { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
-export function buildDevServer({port,}: IBuildOptions): DevServerConfiguration {
-    return {
-        port,
-        open: true,
-        historyApiFallback: true,
-    };
+export function buildDevServer({
+  port,
+}: IBuildOptions): DevServerConfiguration {
+  return {
+    port,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+  };
 }
