2faa0aa90fb95e7562476b4fac80ece201573ffe
Александр Приставка <39094938+alex8073@users.noreply.github.com>
2023-03-09 10:02:43 +0300

    29 Модальное окно. Portal. (#3)
    * добавлена модалка
    
    * добавлена портал, модалка рендерится через портал (темы для модалки будут исправляться)
    
    * обновлены скриншоты

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,11 +1,41 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import React, { FC } from 'react';
+import React, { FC, useCallback, useState } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './NavBar.module.scss';
 
 interface INavBarProps {
   className?: string;
 }
 
-export const NavBar: FC<INavBarProps> = () => (
-    <div className={classNames(cls.NavBar, {}, [])} />
-);
+export const NavBar: FC<INavBarProps> = ({ className }) => {
+    const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState<boolean>(false);
+
+    const onToggleModal = useCallback(() => setIsAuthModal((prev) => !prev), []);
+
+    return (
+        <div className={classNames(cls.NavBar, {}, [className])}>
+            <Button theme={ButtonTheme.CLEAR_INVERTED} className={cls.links} onClick={onToggleModal}>
+                {t('Log in')}
+            </Button>
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                A, animi aspernatur assumenda cum eligendi esse hic incidunt
+                inventore itaque laboriosam mollitia neque nisi quasi quia repellendus
+                similique, velit. Animi architecto earum excepturi in nemo optio
+                praesentium totam voluptate? Ab ad dignissimos expedita laborum
+                molestiae neque nesciunt nihil sed totam voluptate. Ab aliquid
+                debitis doloremque, error impedit in ipsum laborum, perspiciatis
+                provident quibusdam, rerum sunt temporibus? Adipisci, dolor provident
+                quam quibusdam rem rerum. Ab accusantium aspernatur consectetur
+                dignissimos exercitationem laboriosam maxime minima nihil nostrum
+                numquam obcaecati odit perspiciatis, praesentium quae quis quo,
+                repellat soluta sunt? In necessitatibus porro sapiente sit voluptate!
+            </Modal>
+
+        </div>
+    );
+};
