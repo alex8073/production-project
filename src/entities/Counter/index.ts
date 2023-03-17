ec825e7bf3bc4a0204fd845cd4af8d82d1144daf
AlexandrPristavka <alexandr.pristavka@winfinity.live>
2023-03-18 00:06:20 +0300

    fix: изменен импорт

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,5 +1,5 @@
 import { counterReducer } from './model/slice/counterSlice';
-import type { CounterSchema } from './model/types/CounterSchema';
+import { CounterSchema } from './model/types/CounterSchema';
 import { Counter } from './ui/Counter';
 
 export {
