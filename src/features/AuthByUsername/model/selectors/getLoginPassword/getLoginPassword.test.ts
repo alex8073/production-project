5e8640dbf4ad0d1492a8f4a1139b3e187894b9b8
Александр Приставка <39094938+alex8073@users.noreply.github.com>
2023-03-30 09:56:23 +0300

    36 Тестирование фичи authByUsername. TestAsyncThunk (#11)
    * feat: Тестирование фичи authByUsername. TestAsyncThunk
    
    * fix: удален неиспользуемый импорт

---
+++ src/features/AuthByUsername/model/selectors/getLoginPassword/getLoginPassword.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { IStateSchema } from 'app/providers/StoreProvider';
+import { getLoginPassword } from './getLoginPassword';
+
+describe('getLoginPassword.test', () => {
+    test('should return value', () => {
+        const state: DeepPartial<IStateSchema> = {
+            loginForm: {
+                password: '123',
+            },
+        };
+        expect(getLoginPassword(state as IStateSchema)).toEqual('123');
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<IStateSchema> = {};
+        expect(getLoginPassword(state as IStateSchema)).toEqual('');
+    });
+});
---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,31 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { ILoginSchema } from 'features/AuthByUsername';
+import { loginActions, loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+import { loginByUsername } from 'features/AuthByUsername/model/services/loginByUsername/loginByUsername';
+
+describe('loginSlice.test', () => {
+    test('test set username', () => {
+        const state: DeepPartial<ILoginSchema> = {
+            username: '123',
+        };
+        expect(loginReducer(state as ILoginSchema, loginActions.setUsername('new name'))).toEqual({
+            username: 'new name',
+        });
+    });
+    test('test set password', () => {
+        const state: DeepPartial<ILoginSchema> = {
+            password: '123',
+        };
+        expect(loginReducer(state as ILoginSchema, loginActions.setPassword('new password'))).toEqual({
+            password: 'new password',
+        });
+    });
+    test('test set isLoading', () => {
+        const state: DeepPartial<ILoginSchema> = {
+            isLoading: false,
+        };
+        expect(loginReducer(state as ILoginSchema, loginByUsername.pending)).toEqual({
+            isLoading: true,
+        });
+    });
+});
