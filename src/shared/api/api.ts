c40f77a636e4817bd1640459d2df083e8f18dca4
AlexandrPristavka <alexandr.pristavka@winfinity.live>
2023-04-04 01:57:28 +0300

    fix: исправлен импорт для сторибука

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
+// Не видит абсолютный импорт, временное решение
+// import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
+import { USER_LOCALSTORAGE_KEY } from '../const/localStorage';
 
 export const api = axios.create({
     baseURL: __API__,
