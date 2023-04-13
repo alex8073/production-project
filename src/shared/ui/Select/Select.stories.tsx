f496513c01330e9301c370f49d54427e4e948ea5
AlexandrPristavka <alexandr.pristavka@winfinity.live>
2023-04-13 22:52:37 +0300

    feat: добавлен компонент Select

---
+++ src/shared/ui/Select/Select.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Select } from './Select';
+
+export default {
+    title: 'shared/Select',
+    component: Select,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Select>;
+
+const Template: ComponentStory<typeof Select> = (args) => <Select {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    label: 'Label',
+    options: [
+        {
+            value: 'value 1',
+            content: 'value 1',
+        },
+        {
+            value: 'value 2',
+            content: 'value 2',
+        },
+        {
+            value: 'value 3',
+            content: 'value 3',
+        },
+    ],
+};
