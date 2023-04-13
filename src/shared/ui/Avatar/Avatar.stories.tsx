e76a2d4eb6a2e4aa07df0a92b5e197b293cb8ae3
AlexandrPristavka <alexandr.pristavka@winfinity.live>
2023-04-13 22:52:01 +0300

    fix: удалены лишние атрибуты у Avatar в сторибуке

--- src/shared/ui/Avatar/Avatar.stories.tsx
+++ src/shared/ui/Avatar/Avatar.stories.tsx
@@ -9,9 +9,6 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-    args: {
-        to: '',
-    },
 } as ComponentMeta<typeof Avatar>;
 
 const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
