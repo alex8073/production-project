6e92283ad1f6d37c5538e212165357701bfdf196
AlexandrPristavka <alexandr.pristavka@winfinity.live>
2023-12-17 15:08:50 +0300

    fix: update storybook cases

--- src/features/ArticleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/ArticleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -1,16 +1,65 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import withMock from "storybook-addon-mock";
 import ArticleRating from "./ArticleRating";
+import { StoreDecorator } from "@/shared/config/storybook/StoreDecorator/StoreDecorator";
+import {
+    Normal,
+} from "@/features/ArticleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.stories";
 
 export default {
-    title: "shared/ArticleRating",
+    title: "features/ArticleRating",
     component: ArticleRating,
     argTypes: {
         backgroundColor: { control: "color" },
     },
+    decorators: [withMock],
 } as ComponentMeta<typeof ArticleRating>;
 
 const Template: ComponentStory<typeof ArticleRating> = (args) => <ArticleRating {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+    articleId: "1",
+};
+Default.decorators = [StoreDecorator({
+    user: {
+        authData: { id: "1" },
+    },
+})];
+Default.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/article-ratings?userId=1&articleId=1`,
+            method: "GET",
+            status: 200,
+            response: [
+                {
+                    rate: 4,
+                },
+            ],
+        },
+    ],
+};
+
+export const WithoutRate = Template.bind({});
+WithoutRate.args = {
+    articleId: "1",
+};
+WithoutRate.decorators = [
+    StoreDecorator({
+        user: {
+            authData: { id: "1" },
+        },
+    }),
+];
+WithoutRate.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/article-ratings?userId=1&articleId=1`,
+            method: "GET",
+            status: 200,
+            response: [],
+        },
+    ],
+};
