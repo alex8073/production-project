2870a9d5d9a07475b00e9629ff8b2b0c777bafa4
Александр Приставка <39094938+alex8073@users.noreply.github.com>
2024-05-28 22:33:09 +0300

    125.2 концепция Feature flags. Постепенное внедрение новых фичей (#96)

--- src/entities/User/model/types/user.ts
+++ src/entities/User/model/types/user.ts
@@ -1,10 +1,12 @@
 import { UserRole } from "../consts/consts";
+import { IFeatureFlags } from "@/shared/types/featureFlags";
 
 export interface IUser {
     id: string;
     username: string;
     avatar?: string;
     roles?: UserRole[];
+    features?: IFeatureFlags;
 }
 
 export interface IUserSchema {
