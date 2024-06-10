7aec4d7bc2b768f22ffd94020f0fbc31649dac18
Александр Приставка <39094938+alex8073@users.noreply.github.com>
2024-06-10 21:47:00 +0300

    129 Практическое применение json settings. Работа с новыми пользователями (#100)

--- src/entities/User/model/types/jsonSettings.ts
+++ src/entities/User/model/types/jsonSettings.ts
@@ -3,5 +3,5 @@ import { Theme } from "@/shared/const/theme";
 export interface IJsonSettings {
     theme?: Theme;
     isFirstVisit?: true;
-    settingsPageHasBeenOpen?: false;
+    isArticlesPageWasOpened?: boolean;
 }
